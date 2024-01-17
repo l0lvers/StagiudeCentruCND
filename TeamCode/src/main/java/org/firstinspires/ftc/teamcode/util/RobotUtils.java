cbd64641bf0e02118c8db8e8383a5df4a861665b
eupebat <lorena.illes09@gmail.com>
2024-01-17 21:26:54 +0200

    Update RobotUtils.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotUtils.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/RobotUtils.java
@@ -8,7 +8,7 @@ import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 public class RobotUtils {
     private SampleMecanumDrive drive;
-    private DcMotor uppies1;
+    private DcMotor uppies1; //=slider
     private DcMotor uppies2;
     private DcMotor intake1;
     private DcMotor intake2;
@@ -16,8 +16,7 @@ public class RobotUtils {
     private Servo cuva2;
     private Servo opritoare;
 
-    public RobotUtils(HardwareMap hardwareMap)
-    {
+    public RobotUtils(HardwareMap hardwareMap) {
         drive = new SampleMecanumDrive(hardwareMap);
 
         uppies1 = hardwareMap.get(DcMotor.class, "uppies1");
@@ -28,17 +27,16 @@ public class RobotUtils {
         cuva2 = hardwareMap.get(Servo.class, "cuva2");
         opritoare = hardwareMap.get(Servo.class, "opritoare");
     }
-    public void intake(){
-        intake1.setPower(0.7);
-        intake2.setPower(0.7);
+    public void intake() {
+        intake1.setPower(0.5);
+        intake2.setPower(0.5);
     }
-    public void setPosCuva0()
-    {
+    public void setPosCuva0() {
         cuva1.setPosition(0);
         cuva2.setPosition(0);
     }
-    public void setPosOpritoare()
-    {
+    public void setPosOpritoare() {
         opritoare.setPosition(0);
     }
+
 }
