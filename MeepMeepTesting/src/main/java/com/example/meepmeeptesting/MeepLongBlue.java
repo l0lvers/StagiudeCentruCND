fd2fe35de2c8fa98c9226768593fbc3499deb325
l0lvers <elhajsleimanjad@gmail.com>
2024-01-28 01:06:42 +0200

    traiectorie longblue

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepLongBlue.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepLongBlue.java
@@ -6,6 +6,8 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import jdk.tools.jlink.internal.plugins.VendorVersionPlugin;
+
 public class MeepLongBlue{
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(700);
@@ -16,10 +18,22 @@ public class MeepLongBlue{
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-36,61,Math.toRadians(90)))
                         .strafeTo(new Vector2d(-46, 39))
                         .waitSeconds(0.1)
-                        .lineTo(new Vector2d(-46, 56))
-                        .strafeTo(new Vector2d(-40, 56))
-
-
+                        .lineTo(new Vector2d(-46, 58))
+                        .lineToLinearHeading(new Pose2d(38,58,Math.toRadians(0)))
+                        .strafeTo(new Vector2d(45, 42))
+                        .waitSeconds(3)
+                        .lineTo(new Vector2d(42, 42))
+                        .strafeTo(new Vector2d(34,34))
+                        .lineTo(new Vector2d(-50,34))
+                        .strafeTo(new Vector2d(-50,35))
+                        .lineTo(new Vector2d(-61, 35))
+                        .waitSeconds(3)
+                        .lineTo(new Vector2d(45,35))
+                        .waitSeconds(3)
+                        .lineTo(new Vector2d(42, 35))
+                        .strafeTo(new Vector2d(42,13))
+                        .lineTo(new Vector2d(61,13))
+                        .waitSeconds(3)
                         .build()
                 );
 
