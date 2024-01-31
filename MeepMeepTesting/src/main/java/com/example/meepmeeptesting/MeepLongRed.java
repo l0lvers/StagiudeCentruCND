6814bfcd9ae12f373fb91b025781bd6562b0fca2
sebo2701 <lorena.illes09@gmail.com>
2024-01-31 12:09:20 +0200

    meep meep traiectorii

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepLongRed.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepLongRed.java
@@ -6,17 +6,29 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepLongRed {
+import jdk.tools.jlink.internal.plugins.VendorVersionPlugin;
+
+public class MeepLongRed{
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(700);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-36,-61,Math.toRadians(90)))
-
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-36,-61,Math.toRadians(-90)))
+                        //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                        .lineToLinearHeading(new Pose2d(-36,-35,Math.toRadians(-90)))
+                        .lineToLinearHeading(new Pose2d(-36,-60,Math.toRadians(-90)))
+                        .lineToLinearHeading(new Pose2d(-60,-7, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(42,-7, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(42,-35, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(46, -35, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(42,-35,Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(42,-10,Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(60,-10, Math.toRadians(0)))
                         .build()
                 );
+
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
@@ -25,5 +37,3 @@ public class MeepLongRed {
                 .start();
     }
 }
-
-
