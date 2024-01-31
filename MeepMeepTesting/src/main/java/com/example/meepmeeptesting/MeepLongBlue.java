0ce1f9cc5d4e070ed5dfc35fcd5eddd20e1404a9
sebo2701 <lorena.illes09@gmail.com>
2024-01-31 16:58:14 +0200

    meeps lorena
    aaaaaaa

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepLongBlue.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepLongBlue.java
@@ -7,7 +7,6 @@ import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 import jdk.tools.jlink.internal.plugins.VendorVersionPlugin;
-
 public class MeepLongBlue{
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(700);
@@ -15,27 +14,21 @@ public class MeepLongBlue{
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-36,61,Math.toRadians(90)))
-                        .lineTo(new Vector2d(-46, 39))
-                        .waitSeconds(0.1)
-                        .lineTo(new Vector2d(-46, 58))
-                        .lineToLinearHeading(new Pose2d(38,58,Math.toRadians(0)))
-                        .lineTo(new Vector2d(45, 42))
-                        .waitSeconds(3)
-                        .lineTo(new Vector2d(42, 42))
-                        .lineTo(new Vector2d(34,34))
-                        .lineTo(new Vector2d(-50,34))
-                        .lineTo(new Vector2d(-50,35))
-                        .lineTo(new Vector2d(-61, 35))
-                        .waitSeconds(3)
-                        .lineTo(new Vector2d(45,35))
-                        .waitSeconds(3)
-                        .lineTo(new Vector2d(42, 35))
-                        .lineTo(new Vector2d(42,13))
-                        .lineTo(new Vector2d(61,13))
-                        .waitSeconds(3)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(-36,61,Math.toRadians(0)))
+                        //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                        .lineToLinearHeading(new Pose2d(-36,35,Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(-36,60,Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(-53,60, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(-53,7, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(42,7, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(42,35, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(46, 35, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(42,35,Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(42,10,Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(60,10, Math.toRadians(0)))
                         .build()
                 );
+
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepShortBlue.java
@@ -0,0 +1,34 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.geometry.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepShortBlue {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(700);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(90), 15)
+                .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(11,61,Math.toRadians(90)))
+                        //AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+                        .lineToLinearHeading(new Pose2d(11,34,Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(11,40,Math.toRadians(90)))
+                        .lineToLinearHeading(new Pose2d(40,40, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(46,34, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(41,34, Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(41,10,Math.toRadians(0)))
+                        .lineToLinearHeading(new Pose2d(60,10, Math.toRadians(0)))
+                        .build()
+                );
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
