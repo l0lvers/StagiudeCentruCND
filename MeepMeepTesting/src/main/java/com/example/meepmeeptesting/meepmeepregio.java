e6e873ddf764f9e0abade74e71942322c1905e06
Sebo2701 <sevastianpetru.popa@gmail.com>
2024-02-14 16:58:58 +0200

    ff

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/meepmeepregio.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/meepmeepregio.java
@@ -7,7 +7,7 @@ import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 public class meepmeepregio
-
+//-9 -70
 {
     public static void main(String[] args) {
         MeepMeep meepMeep = new MeepMeep(700);
