55abaff765b272eed9aaad883d8ea3a38181ef0c
Zxeniku <97277701+l0lvers@users.noreply.github.com>
2024-02-15 19:09:08 +0200

    .

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepShortRedCenter.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepShortRedCenter.java
@@ -6,6 +6,8 @@ import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
+import java.util.Vector;
+
 public class MeepShortRedCenter
 //-9 -70
 {
@@ -16,29 +18,28 @@ public class MeepShortRedCenter
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive -> drive.trajectorySequenceBuilder(new Pose2d(11,-61,Math.toRadians(90)))
-                        .splineTo(new Vector2d(48, -35), Math.toRadians(0))
+                        .lineToSplineHeading(new Pose2d(48, -35, Math.toRadians(0)))
                         .lineToConstantHeading(new Vector2d(25, -24))
                         .waitSeconds(1)
                         //stack 1
-                        .lineToConstantHeading(new Vector2d(35, -59))
+                        .lineToConstantHeading(new Vector2d(25, -59))
                         .lineToConstantHeading(new Vector2d(-14, -59))
                         .lineToLinearHeading(new Pose2d(-16, -59, Math.toRadians(-23)))
                         .waitSeconds(1)
                         //score 2
                         .lineToLinearHeading(new Pose2d(-14, -59, Math.toRadians(0)))
-                        .lineToConstantHeading(new Vector2d(20, -59))
-                        .splineTo(new Vector2d(48, -35), Math.toRadians(0))
+                        .lineToConstantHeading(new Vector2d(30, -59))
+                        .lineToConstantHeading(new Vector2d(48, -35))
                         //stack 2
-                        .lineToLinearHeading(new Pose2d(35, -35, Math.toRadians(0)))
-                        .splineToConstantHeading(new Vector2d(13, -59), Math.toRadians(-90))
+                        .lineToConstantHeading(new Vector2d(30, -59))
                         .lineToConstantHeading(new Vector2d(-14, -59))
                         .lineToLinearHeading(new Pose2d(-16, -59, Math.toRadians(-23)))
                         .waitSeconds(1)
                         //score 2
                         .lineToLinearHeading(new Pose2d(-14, -59, Math.toRadians(0)))
-                        .lineToConstantHeading(new Vector2d(20, -59))
-                        .splineTo(new Vector2d(48, -35), Math.toRadians(0))
-                        .lineToLinearHeading(new Pose2d(41, -41, Math.toRadians(0)))
+                        .lineToConstantHeading(new Vector2d(30, -59))
+                        .lineToConstantHeading(new Vector2d(48, -35))
+                        .lineToLinearHeading(new Pose2d(41, -35, Math.toRadians(0)))
                         //park
                         .lineToConstantHeading(new Vector2d(41, -59))
                         .lineToConstantHeading(new Vector2d(57, -59))
